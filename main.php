a2d496790a9dfc808b6a4f80037ea924eee0b095
dadaOveride <nick@lineindex.info>
2020-06-23 19:22:30 -0400

    example request

--- main.php
+++ main.php
@@ -1,10 +1,20 @@
 <?php
 
+# Include Dependencies From Composer
+
+include 'vendor/autoload.php';
+
 # ROOT FILE FOR STRIPE APPLICATION
 
 include 'secrets/get-keys.php';
+include 'secrets/variables.php';
 
-
+\Stripe\PaymentIntent::create([
+'amount' => 1000,
+'currency' => 'usd',
+'payment_method_types' => ['card'],
+'receipt_email' => $email,
+]);
 
 
 
